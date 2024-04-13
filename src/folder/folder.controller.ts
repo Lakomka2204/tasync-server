976ee908f83f813e848e1f4eeb1a37e69888a2de
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-13 10:27:47 +0000

    added very simple file upload & get

--- src/folder/folder.controller.ts
+++ src/folder/folder.controller.ts
@@ -1,4 +1,4 @@
-import {  BadRequestException, Body, Controller, Get, HttpCode, NotFoundException, Param, Post, Put, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import {  BadRequestException, Body, Controller, Delete, Get, HttpCode, MaxFileSizeValidator, NotFoundException, Param, ParseFilePipe, Post, Put, Req, StreamableFile, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor} from '@nestjs/platform-express';
 import { Request } from 'express';
 import { join } from 'path';
@@ -14,7 +14,7 @@ import { Throttle, minutes } from '@nestjs/throttler';
 @UseGuards(AccountGuard)
 export class FolderController {
     constructor(
-        private readonly fileService: FolderService
+        private readonly folderService: FolderService
     ){}
     @Get(":owner/:name")
     @HttpCode(200)
@@ -23,7 +23,7 @@ export class FolderController {
         @Param('owner') ownerName: string,
         @Param('name') folderName: string,
     ): Promise<FolderInfoDto> {
-        const folder = await this.fileService.getFolderByName(ownerName,folderName);
+        const folder = await this.folderService.getFolderByName(ownerName,folderName);
         if (!folder)
             throw new NotFoundException("Folder is not found");
         if (folder?.isPublic)
@@ -42,21 +42,60 @@ export class FolderController {
         @Body() createFolderBody: CreateFolderDto
     ) {
         const account: Account = JSON.parse(req.headers.authorization);
-        const folder = await this.fileService.createFolder(account,createFolderBody);
+        const folder = await this.folderService.createFolder(account,createFolderBody);
         if (!folder) throw new BadRequestException(`Folder with specified name already exists`);
         return folder.id;
 
     }
 
-    @Post(':owner/:name')
+    @Delete(':name')
+    @HttpCode(204)
+    async deleteFolder(
+        @Req() req: Request,
+        @Param('name') folderName: string
+    ) {
+        const account: Account = JSON.parse(req.headers.authorization);
+        const isDeleted = await this.folderService.deleteFolder(account,folderName);
+        if (!isDeleted)
+            throw new NotFoundException("Folder is not found");
+    }
+
+    @Put(":name")
+    @HttpCode(201)
+    @Throttle({default: {ttl:minutes(1), limit:5}})
     @UseInterceptors(FileInterceptor('file'))
-    async uploadArchive(
-        @Param('name') folderName: string,
-        @Param('owner') ownerName: string,
-        @UploadedFile()
+    async uploadFile(
+        @Req() req: Request,
+        @Param("name") folderName: string,
+        @UploadedFile(new ParseFilePipe({
+            validators:[
+                new MaxFileSizeValidator({maxSize: 104_857_600,message:"Too large file"})
+            ]
+        }))
         file: Express.Multer.File
     ) {
-        if (file.originalname !== ".tasync") throw new BadRequestException("Invalid file type");
-        return join(ownerName,folderName);
+        const account: Account = JSON.parse(req.headers.authorization);
+        if (!file)
+            throw new BadRequestException("File is required");
+        const isCreated = await this.folderService.createFile(account.username,folderName,file);
+        if (!isCreated)
+            throw new NotFoundException("Folder is not found");
+
+    }
+    @Get(":owner/:name/:file")
+    @HttpCode(200)
+    @Throttle({default: {ttl:minutes(5), limit:50}})
+    async getFile(
+        @Req() req: Request,
+        @Param('owner') ownerFolderName: string,
+        @Param('name') folderName: string,
+        @Param('file') fileName: string,
+    ): Promise<StreamableFile> {
+        const account: Account = JSON.parse(req.headers.authorization);
+        const file = await this.folderService.getFile(account,ownerFolderName,folderName,fileName);
+        if (!file)
+            throw new NotFoundException("File not found")
+
+        return new StreamableFile(file,{length:file.length});
     }
 }
--- src/folder/folder.service.ts
+++ src/folder/folder.service.ts
@@ -4,9 +4,10 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateFolderDto } from './dto/create-folder.dto';
 import { Account } from 'src/account/account.entity';
-import { mkdir } from 'fs/promises';
+import { mkdir, readFile, writeFile } from 'fs/promises';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
+import { existsSync } from 'fs';
 
 @Injectable()
 export class FolderService {
@@ -22,21 +23,44 @@ export class FolderService {
                     username: owner
                 }
             },
-            relations:['owner'],
-            select:{
+            relations: ['owner'],
+            select: {
                 owner: {
-                    id:true
+                    id: true
                 }
             }
         });
     }
-    async createFolder(owner: Account, {name,isPublic}:CreateFolderDto): Promise<Folder | null> {
+    async getFolders(owner: Account): Promise<Folder[]> {
+        return await this.folderRepo.findBy({ owner: { id: owner.id } });
+    }
+    async createFolder(owner: Account, { name, isPublic }: CreateFolderDto): Promise<Folder | null> {
         const dbFolder = await this.folderRepo.findOne({ where: { name, owner: { id: owner.id } } });
         if (dbFolder)
             return null;
-        const folder = this.folderRepo.create({ name, owner: { id: owner.id },isPublic });
-        const newFolderPath = join(this.config.getOrThrow('TMP_FILE_STORAGE'),owner.username,name);
+        const folder = this.folderRepo.create({ name, owner: { id: owner.id }, isPublic });
+        const newFolderPath = join(this.config.getOrThrow('TMP_FILE_STORAGE'), owner.username, name);
         await mkdir(newFolderPath)
         return await this.folderRepo.save(folder);
+    }
+    async deleteFolder(owner: Account, folderName: string): Promise<boolean> {
+        const folder = await this.folderRepo.findOne({ where: { name: folderName, owner: { id: owner.id } } });
+        if (!folder)
+            return false;
+        return (await this.folderRepo.softDelete({ id: folder.id })).affected > 0;
+    }
+    async getFile(requestor: Account, ownerFolderName: string, folderName: string, fileName: string): Promise<Buffer | null> {
+        const folder = await this.getFolderByName(ownerFolderName, folderName);
+        if (!(folder.isPublic || folder.owner.id === requestor.id))
+            return null;
+        const filePath = join(this.config.getOrThrow("TMP_FILE_STORAGE"), ownerFolderName, folderName, fileName);
+        return await readFile(filePath);
+    }
+    async createFile(ownerFolderName: string, folderName: string, file: Express.Multer.File): Promise<boolean> {
+        const folderPath = join(this.config.getOrThrow("TMP_FILE_STORAGE"), ownerFolderName, folderName);
+        const filePath = join(folderPath, file.originalname);
+        if (!existsSync(folderPath)) return false;
+        await writeFile(filePath, file.buffer);
+        return true;
     }
 }
