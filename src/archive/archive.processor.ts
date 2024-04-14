815f62049d816b3e3e33afda533620d2b73884ea
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-14 15:02:00 +0000

    united archive method into one because onfinishedqueue was waiting for some reason

--- src/archive/archive.processor.ts
+++ src/archive/archive.processor.ts
@@ -7,28 +7,26 @@ import { ConfigService } from '@nestjs/config';
 import { unlink, writeFile } from 'fs/promises';
 import * as AdmZip from 'adm-zip';
 import { Constants } from 'adm-zip/util';
+import { FsService } from 'src/fs/fs.service';
+import { setTimeout } from 'timers/promises';
 @Processor('archive')
 export class ArchiveProcessor {
     constructor(
-        private readonly config: ConfigService
+        private readonly config: ConfigService,
+        private readonly fsService: FsService
     ){}
     @Process('archive')
     async archiveFile(job: Job<ArchiveJob>) {
+        if (this.fsService.exists(job.data.location))
+            await this.fsService.deleteFile(job.data.location);
+        await setTimeout(10000);
         var zip = new AdmZip(undefined,{method:Constants.LZMA});
         for(const file of job.data.items) {
-            console.log("Processing file %s %s",file.filename,file.location)
             zip.addLocalFile(file.location,undefined,file.filename);
         }
-
-        return await zip.toBufferPromise();
-    }
-
-    @OnQueueCompleted({name:"archive"})
-    async uploadArchiveToFileStorage(job:Job<ArchiveJob>) {
-        console.log('finished job!!!!');
-        const buffer: Buffer = job.returnvalue;
-        await writeFile('/tmp/archive.zip',buffer);
+        const buffer =  await zip.toBufferPromise();
+        await this.fsService.createFile(job.data.location,buffer);
         for(const file of job.data.items)
-            await unlink(file.location);
+            await this.fsService.deleteFile(file.location);
     }
 }
