9fb23213a2858c1933c49f490a7b4edf34088748
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-14 15:02:21 +0000

    added fs module to interact with filesystem with service and not directly

--- src/archive/archive.module.ts
+++ src/archive/archive.module.ts
@@ -2,9 +2,10 @@ import { BullModule } from "@nestjs/bull";
 import { Module } from "@nestjs/common";
 import { ArchiveService } from "./archive.service";
 import { ArchiveProcessor } from "./archive.processor";
+import { FsModule } from "src/fs/fs.module";
 
 @Module({
-    imports: [BullModule.registerQueue({name:"archive"})],
+    imports: [BullModule.registerQueue({name:"archive"}),FsModule],
     exports: [ArchiveService,ArchiveProcessor],
     providers: [ArchiveService,ArchiveProcessor]
 })
