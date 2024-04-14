a11f126002a2926c15d648e9312f16d03123d391
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-14 15:08:55 +0000

    fs = filesystem module

---
+++ src/fs/fs.service.ts
@@ -0,0 +1,33 @@
+
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { existsSync } from "fs";
+import { readFile, unlink, writeFile } from "fs/promises";
+import { join } from "path";
+
+@Injectable()
+export class FsService {
+    constructor(
+        config: ConfigService
+    ){this.baseFolder = config.getOrThrow("TMP_FILE_STORAGE")}
+    baseFolder: string;
+    async getFile(filename: string): Promise<Buffer | null> {
+        const fullPath = join(this.baseFolder,filename);
+        if (!existsSync(fullPath)) return null;
+        return await readFile(fullPath);
+    }
+    async createFile(filename: string, content: Buffer) {
+        const fullPath =join(this.baseFolder,filename);
+        if (existsSync(fullPath))
+            await unlink(fullPath)
+        await writeFile(fullPath,content);
+    }
+    async deleteFile(filename: string) {
+        const fullPath = join(this.baseFolder,filename);
+        if (existsSync(fullPath))
+            await unlink(fullPath)
+    }
+    exists(filename:string) {
+        return existsSync(join(this.baseFolder,filename));
+    }
+}
