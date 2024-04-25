cbecc195682ec8d4e1131968d8a7a182d4515cea
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-25 18:19:50 +0000

    dynamic type for matching context

--- src/db-source.ts
+++ src/db-source.ts
@@ -1,16 +1,16 @@
+import { join } from 'path';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import {config} from 'dotenv';
-config();
+const entityExt = __dirname.endsWith("src") ? 'ts' : 'js';
 export const dbSource: DataSourceOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    schema: process.env.DB_SCHEME,
-    database: process.env.DB_DATABASE,
-    entities: [process.env.TYPEORM_ENTITIES],
-    migrations: [process.env.TYPEORM_MIGRATIONS],
-    synchronize: process.env.NODE_ENV == 'development',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  schema: process.env.DB_SCHEME,
+  database: process.env.DB_DATABASE,
+  entities: [join(__dirname, '**', `*.entity.${entityExt}`)],
+  migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+  migrationsRun: true
 };
 export const ds = new DataSource(dbSource);
